d1d3b30e3c1c28da317a0a6c5c78cbec9188c071
VikVanDik <95vlc17@gmail.com>
2024-06-05 17:29:10 +0200

    audio updated

--- pong_v0.11/main.lua
+++ pong_v0.11/main.lua
@@ -45,6 +45,14 @@ function love.load()
     -- creiamo un font più grande che servirà per il risultato
     scoreFont = love.graphics.newFont('font.ttf', 30)
 
+    -- impostiamo i suoni del gioco
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['winner'] = love.audio.newSource('sounds/winner.mp3', 'static')
+    }
+
     -- Per settare la larghezza della finestra non usiamo più la funzione window.setmode ma utilizziamo la libreria push
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -103,6 +111,7 @@ function love.update(dt)
                 ball.dy = math.random(10, 150)
             end
             
+            sounds['paddle_hit']:play()
         end
 
         
@@ -119,14 +128,17 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            sounds['paddle_hit']:play()
         end
 
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds['score']:play()
             if player2Score == 3 then 
                 winningPlayer = 2
                 gameState = "winning"
+                sounds['winner']:play()
             else
                 gameState = 'serve'
                 ball:reset()
@@ -136,9 +148,11 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds['score']:play()
             if player1Score == 3 then 
                 winningPlayer = 1
                 gameState = "winning"
+                sounds['winner']:play()
             else
                 gameState = 'serve'
                 ball:reset()
@@ -149,11 +163,14 @@ function love.update(dt)
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = - ball.dy
+            sounds['wall_hit']:play()
         end
     
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
+
         end
     end
     

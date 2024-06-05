63993f6836ecc31a32df7b740a2d01024b339b1a
VikVanDik <95vlc17@gmail.com>
2024-06-05 17:09:14 +0200

    update 0.11

---
+++ pong_v0.11/main.lua
@@ -0,0 +1,270 @@
+
+-- Prendiamo la libreria push, serve a dare un tocco retrò al gioco dandoci delle dimensioni virtuali della finestra, la finestra avrà comunque altezza e larghezza scelte ma renderizzerà il gioco alle dimensioni virtuali.
+-- Link della libreria: https://github.com/Ulydev/push
+push = require 'push'
+
+
+-- libreria class, utilizzata per semplificare l'utilizzo delle classi
+-- link della libreria: https://github.com/vrld/hump/blob/master/class.lua
+Class = require 'class'
+
+-- richiamiamo la classe Paddle che contiene tutte le info dei Paddle
+require 'Paddle'
+
+-- facciamo la stessa cosa con la palla
+require 'Ball'
+
+-- Variabili di sistema utilizzate dalla funzione love.window.setmode per definire la grandezza della finestra
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- Variabili della renderizzazione virtuale
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- Creiamo un valore di velocità dei rettangoli
+PADDLE_SPEED = 200
+
+-- funzione love.load() viene chiamata solo quando il gioco parte, serve per inizializzare il gioco.
+
+function love.load()
+
+    -- il filtro nearest ci permette di rimuovere l'effetto offuscato dal testo, rimuovere per provare
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+
+    -- settiamo il nome del gioco
+    love.window.setTitle('Pong')
+    -- facciamo un seed del randomizer (ha bisogno di un valore per funzionare)
+    -- utilizziamo il tempo attuale in secondi dal 01/01/1970
+    math.randomseed(os.time())
+
+    -- creiamo una variabile font prendendolo dal file nella directory, il numero indica la grandezza del font
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- creiamo un font più grande che servirà per il risultato
+    scoreFont = love.graphics.newFont('font.ttf', 30)
+
+    -- Per settare la larghezza della finestra non usiamo più la funzione window.setmode ma utilizziamo la libreria push
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    -- inizializzo due variabili per il punteggio dei due giocatori
+    player1Score = 0
+    player2Score = 0
+
+    -- giocatore che serve, default 1
+    servingPlayer = 1
+
+    -- carichiamo i player (paddles)
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10,VIRTUAL_HEIGHT - 30, 5, 20)
+
+    -- carichiamo la palla
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    -- creo una variabile di stato del gioco, per dividere i vari momenti
+    -- start è il momento in cui ancora non si gioca ed è tutto fermo
+    gameState = 'start'
+end
+
+
+
+function love.update(dt)
+    
+    if gameState == 'serve' then
+
+        -- il delta della velocità y viene dato in maniera casuale
+        ball.dy = math.random(-50, 50)
+        -- dipendentemente dal giocatore che ha segnato facciamo partire la pallina dall'altro lato
+        if servingPlayer == 1 then
+            ball.dx = math.random(140, 200)
+        else
+            ball.dx = -math.random(140, 200)
+        end
+        
+
+    elseif gameState == 'play' then
+        -- becchiamo le collisioni tramite la funzione nel Ball.lua
+        if ball:collide(player1) then
+            -- invertiamo il delta x della palla e lo velocizziamo
+            ball.dx = -ball.dx * 1.2
+            -- ci assicuriamo che la palla non entri all'interno del paddle dandogli una posizione
+            ball.x = player1.x + 5
+            
+            --variamo la y della palla in modo da non avere sempre solo lo stesso angolo
+
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+            
+        end
+
+        
+        -- becchiamo le collisioni tramite la funzione nel Ball.lua con il player 2
+        if ball:collide(player2) then
+            -- invertiamo il delta x della palla e lo velocizziamo
+            ball.dx = -ball.dx * 1.02
+            -- ci assicuriamo che la palla non entri all'interno del paddle dandogli una posizione
+            -- in questo caso -4 perché partendo da destra incontriamo la larghezza della palla
+            ball.x = player2.x - 4
+            
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+        end
+
+        if ball.x < 0 then
+            servingPlayer = 1
+            player2Score = player2Score + 1
+            if player2Score == 3 then 
+                winningPlayer = 2
+                gameState = "winning"
+            else
+                gameState = 'serve'
+                ball:reset()
+            end
+        end
+        
+        if ball.x > VIRTUAL_WIDTH then
+            servingPlayer = 2
+            player1Score = player1Score + 1
+            if player1Score == 3 then 
+                winningPlayer = 1
+                gameState = "winning"
+            else
+                gameState = 'serve'
+                ball:reset()
+            end
+        end
+
+        -- adesso consideriamo le collisioni con le parti sopra e sotto dello schermo
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = - ball.dy
+        end
+    
+        if ball.y >= VIRTUAL_HEIGHT - 4 then
+            ball.y = VIRTUAL_HEIGHT - 4
+            ball.dy = -ball.dy
+        end
+    end
+    
+    
+    -- movimenti player 1
+    if love.keyboard.isDown('w') then
+        -- aggiungo velocità negativa al valore y
+        player1.dy = -PADDLE_SPEED
+        
+    elseif love.keyboard.isDown('s') then
+        -- aggiungiamo velocità positiva al valore y
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+    
+    --movimenti player 2 
+    if love.keyboard.isDown('up') then
+        player2.dy = - PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+    
+    if gameState == 'play' then
+        ball:update(dt)
+    end
+    
+    player1:update(dt)
+    player2:update(dt)
+end
+
+-- funzione love.pressedkey() serve a intercettare gli input degli utenti
+function love.keypressed(key)
+    
+    if key == 'escape' then
+        love.event.quit()
+    elseif key == 'enter' or key == 'return' then 
+        if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'serve' then
+            gameState = 'play'
+        elseif gameState == 'winning' then
+
+            gameState = 'serve'
+
+            player1Score = 0
+            player2Score = 0
+
+            ball:reset()
+            if winningPlayer == 1 then
+                servingPlayer = 2
+            else
+                servingPlayer = 1
+            end
+        end
+    end
+end
+
+
+-- funzione love.draw() viene chiamata dopo l'update di LOVE2D e serve per disegnare qualcosa sullo schermo.
+
+function love.draw()
+
+    --aggiungiamo la libreria push per renderizzare utilizzando il filtro e la risoluzione virtuale
+    -- tutto quello che c'è tra push:apply('start') e push:apply('end') verrà renderizzato e filtrato secondo la libreria
+    push:apply('start')
+    
+    -- sposto più in alto la scritta ciao pong rispetto allo scorso commit ricordando che adesso le misure rispecchieranno quelle virtuali
+    love.graphics.setFont(smallFont)
+
+    if gameState == 'start' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Benvenuto su Pong!', 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Premi invio per iniziare', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'serve' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Serve il giocatore ' .. tostring(servingPlayer), 
+            0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf('Premi invio per servire', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'play' then
+        -- no UI messages to display in play
+
+    elseif gameState == 'winning' then
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(255/255, 0, 0, 255/255)
+    love.graphics.printf('Vince il giocatore ' .. tostring(servingPlayer), 
+            0, 10, VIRTUAL_WIDTH, 'center')
+    end
+    
+    --scrivo il punteggio a schermo
+    -- setto il font come prima cosa altrimenti utilizza quello precedente
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+
+    -- creiamo i due giocatori, utilizzando le classi adesso
+    player1:render()
+    player2:render()
+    
+    -- facciamo la stessa cosa con la palla
+    ball:render()
+
+    -- scriviamo a schermo gli fps
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255/255, 0, 255/255)
+    love.graphics.printf('FPS: ' .. tostring(love.timer.getFPS()), 10, 10, VIRTUAL_WIDTH, left)
+    
+    push:apply('end') 
+end
+
+
